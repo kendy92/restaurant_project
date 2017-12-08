3d56b22c0ba469441f4e8be9074d5965dc1680fd
kendy92 <dinhconganh@gmail.com>
2017-12-07 23:35:59 -0500

    Update tablet style version

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ $(function(){
     $("#datepicker").datepicker();
     
    //hover effect in top navigation
-    $("ul.top-menu li").hover(
-    function(){
-        $(this).children("ul.child-menu").finish().slideDown('medium');
-    },function(){
-        $(this).children("ul.child-menu").finish().slideUp('medium');
+    
+    var screen_width = $(window).width();
+        if(screen_width > 1024){
+        
+             $("ul.top-menu li").hover(
+                function(){
+                    $(this).children("ul.child-menu").finish().slideDown('medium');
+                },function(){
+                    $(this).children("ul.child-menu").finish().slideUp('medium');
+                });
+            
+        }else{
+            var count = 0;
+            $("ul.top-menu li").on('click',function(){
+                count++;
+                if(count % 2 == 0){
+                    $(this).children("ul.child-menu").finish().slideUp('medium');
+                }else{
+                    $(this).children("ul.child-menu").finish().slideDown('medium');
+                }
+                return false;
+            });
+        }
+    
+    
+    //toggleBtn for mobile and tablet version
+    var count_click = 0;
+    $(".toggleBtn").on("click",function(){
+        count_click++;
+        if(count_click % 2 == 0){
+            $("ul.top-menu").finish().slideUp('medium');
+        }else{
+            $("ul.top-menu").finish().slideDown('medium');
+        }
     });
 });
