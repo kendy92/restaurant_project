5c0519fcdce45abffa326d49fad6b9dacaf4cb13
kendy92 <dinhconganh@gmail.com>
2017-12-07 20:00:28 -0500

    Add hover effect to navigation menu

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 $(function(){
    //init Date picker
     $("#datepicker").datepicker();
+    
+   //hover effect in top navigation
+    $("ul.top-menu li").hover(
+    function(){
+        $(this).children("ul.child-menu").finish().slideDown('medium');
+    },function(){
+        $(this).children("ul.child-menu").finish().slideUp('medium');
+    });
 });
