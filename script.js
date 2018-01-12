0e9315920671820cefe6df8855486fd4f49ccdf6
kendy92 <dinhconganh@gmail.com>
2018-01-11 23:42:34 -0500

    add policy & disclaimer page

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 //update 12/01/2018
-
-//Coded by Aqs
-function dailySpecial(evt, spcName) {
-		var i, tabcontent, tablinks;
-		tabcontent = document.getElementsByClassName("tabcontent");
-		for (i = 0; i < tabcontent.length; i++) {
-			tabcontent[i].style.display = "none";
-		}
-		tablinks = document.getElementsByClassName("tablinks");
-		for (i = 0; i < tablinks.length; i++) {
-			tablinks[i].className = tablinks[i].className.replace(" active", "");
-		}
-		document.getElementById(spcName).style.display = "block";
-		evt.currentTarget.className += " active";
-	}
-
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 1; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
-
-
 //Coded by lee
 $(function(){
 
