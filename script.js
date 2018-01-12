c945f73ce64a4efc1c39419abf29236c081455d1
kendy92 <dinhconganh@gmail.com>
2018-01-11 23:14:15 -0500

    fixed

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-//update 11/01/2018
-//Coded by lee modied by aqs
+//update 12/01/2018
+
+//Coded by Aqs
+function dailySpecial(evt, spcName) {
+		var i, tabcontent, tablinks;
+		tabcontent = document.getElementsByClassName("tabcontent");
+		for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "none";
+		}
+		tablinks = document.getElementsByClassName("tablinks");
+		for (i = 0; i < tablinks.length; i++) {
+			tablinks[i].className = tablinks[i].className.replace(" active", "");
+		}
+		document.getElementById(spcName).style.display = "block";
+		evt.currentTarget.className += " active";
+	}
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 1; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+
+//Coded by lee
 $(function(){
 
     //init Date picker
